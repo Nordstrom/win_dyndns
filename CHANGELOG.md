3d2af8d823d06688dea0533ae76f20ef84ffb098
Dave Viebrock <dave.viebrock@nordstrom.com>
2015-09-21 10:08:14 -0700

    initial release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision History for win_dyndns
 
+## 1.0.0
+
+* initial release, working as-advertised
+
 ## 0.1.1
 
 * rewriting with windows library module, plus minor bug fix
--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer       'EPG WSE Team'
 maintainer_email 'itwseall@nordstrom.com'
 license          'all_rights'
 description      'Configures Windows dynamic DNS registration setting'
-version          '0.1.10'
+version          '1.0.0'
 
 depends 'windows', '~> 1.38'
 depends 'wse_powershell_helper', '~> 0.1'
